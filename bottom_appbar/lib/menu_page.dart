98f038bef8dcbcd44cb5d62938b3f5d3dff46b78
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2020-08-27 13:37:03 -0300

    Changes on bottomNavigationBar

--- bottom_appbar/lib/menu_page.dart
+++ bottom_appbar/lib/menu_page.dart
@@ -1,5 +1,6 @@
 import 'package:bottom_appbar/search_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'home_page.dart';
 
@@ -11,6 +12,7 @@ class MenuBottomBar extends StatefulWidget {
 class _MenuBottomBarState extends State<MenuBottomBar> {
 
   int _currentIndex = 0;
+  bool _isActive = false;
 
   final tabs = <Widget>[
     HomePage(),
@@ -24,26 +26,73 @@ class _MenuBottomBarState extends State<MenuBottomBar> {
         title: Text('BottomMenuTest'),
       ),
       body: tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-            backgroundColor: Colors.deepPurple
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Search'),
-            backgroundColor: Colors.deepPurple
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
+      bottomNavigationBar: Container(
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            MaterialButton(
+              height: MediaQuery.of(context).size.height / 15,
+              minWidth: MediaQuery.of(context).size.width / 2,
+              color: !_isActive ? Colors.deepPurple[50] : Colors.deepPurple,
+              child: Text(
+                'Vendas', 
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: !_isActive ? Colors.deepPurple : Colors.deepPurple[50],
+                ),
+              ),
+              onPressed: (){
+                setState(() {
+                  _currentIndex = 0;
+                  _isActive = false;
+                });
+              },
+            ),
+            MaterialButton(
+              height: MediaQuery.of(context).size.height / 15,
+              minWidth: MediaQuery.of(context).size.width / 2,
+              color: _isActive ? Colors.deepPurple[50] : Colors.deepPurple,
+              child: Text(
+                'Pagamentos', 
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: _isActive ? Colors.deepPurple : Colors.deepPurple[50],
+                ),
+              ),
+              onPressed: (){
+                setState(() {
+                  _currentIndex = 1;
+                  _isActive = true;
+                });
+              },
+            ),
+          ],
+        ),
+        
       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   type: BottomNavigationBarType.shifting,
+      //   fixedColor: Colors.green,
+      //   currentIndex: _currentIndex,
+      //   items: [
+      //     BottomNavigationBarItem(
+      //       backgroundColor: Colors.deepPurple,
+      //       icon: Icon(Icons.home),
+      //       title: Text('Home'),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       backgroundColor: Colors.greenAccent,
+      //       icon: Icon(Icons.search),
+      //       title: Text('Search'),
+      //     ),
+
+      //   ],
+      //   onTap: (index) {
+      //     setState(() {
+      //       _currentIndex = index;
+      //     });
+      //   },
+      // ),
     );
   }
 }
