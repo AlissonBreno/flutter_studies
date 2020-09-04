f47a66e37c49d5115f673cee9a32df29bed85b49
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2020-09-04 16:05:22 -0300

    DateRangepicker Done

--- bottom_appbar/lib/main.dart
+++ bottom_appbar/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'menu_page.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +13,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BottomNavigation Test',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('pt', 'BR')],
+      title: 'BottomNavigation Test',
       home: MenuBottomBar(),
     );
   }
--- bottom_appbar/lib/tabs/homePageTabs/tab1Widgets/data_filter.dart
+++ bottom_appbar/lib/tabs/homePageTabs/tab1Widgets/data_filter.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:date_range_picker/date_range_picker.dart' as DateRagePicker;
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
+class DataFilter extends StatefulWidget {
+  @override
+  _DataFilterState createState() => _DataFilterState();
+}
 
+class _DataFilterState extends State<DataFilter> {
+  DateTime _startDate = DateTime.now();
+  DateTime _endDate = DateTime.now().add(Duration(days: 7));
 
-class DataFilter extends StatelessWidget {
+  Future displayDateRangePicker(BuildContext context) async {
+    final List<DateTime> picked = await DateRagePicker.showDatePicker(
+      context: context, 
+      initialFirstDate: _startDate, 
+      initialLastDate: _endDate,
+      firstDate: new DateTime(DateTime.now().year - 50), 
+      lastDate: new DateTime(DateTime.now().year + 50), 
+      // locale: Locale("pt", "Br"),
+    );
+    if(picked != null && picked.length == 2){
+      setState(() {
+        _startDate = picked[0];
+        _endDate = picked[1];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    initializeDateFormatting('pt_BR', null);
+    var dateFormatter = new DateFormat('dd MMM, yyyy', 'pt_BR');
+    String startDate = dateFormatter.format(_startDate);
+    String endDate = dateFormatter.format(_endDate);
+
+
+
     return OutlineButton(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
       textColor: Colors.deepPurple,
@@ -25,10 +60,11 @@ class DataFilter extends StatelessWidget {
               size: 40.0,
             ),
             Text(
-              '11 Juho, 2020 - 17 Julho, 2020',
+              '$startDate - $endDate',
+              // '11 Junho, 2020 - 17 Julho, 2020',
               style: TextStyle(
                 fontSize: 18.0,
-                fontWeight: FontWeight.w300
+                fontWeight: FontWeight.w300                
               ),
             ),
             Icon(Icons.arrow_drop_down)
@@ -37,8 +73,8 @@ class DataFilter extends StatelessWidget {
       ),
       //TODO make Date Range pick work
       //https://pub.dev/packages/syncfusion_flutter_datepicker
-      onPressed: () {
-        
+      onPressed: () async {
+        await displayDateRangePicker(context);
       },
     );
   }
